e5e52bb45490d86aa17486b9f6a0cb94ebb82ce2
MFavian Zaahir <favian.zahir@gmail.com>
2024-08-10 12:24:46 +0700

    I hate file uploads and error handling

--- src/app/admin/_actions/product.ts
+++ src/app/admin/_actions/product.ts
@@ -2,6 +2,8 @@
 
 import { z } from "zod"
 import db from "@/db/db"
+import fs from 'fs/promises'
+import { redirect } from "next/navigation"
 
 const fileSchema = z.instanceof(File, { message: 'Required'})
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -14,7 +16,7 @@ const addSchema = z.object({
     image: imageSchema.refine(file => file.size > 0, "Required")
 })
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevStat:unknown, formData: FormData) {
     const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
     if(result.success === false) {
         return result.error.formErrors.fieldErrors
@@ -22,11 +24,22 @@ export async function addProduct(formData: FormData) {
 
     const data = result.data
 
-    db.product.create({data: {
+    await fs.mkdir("public/products", { recursive: true })
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(`public{imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+
+    await fs.mkdir("products", { recursive: true })
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+
+    await db.product.create({data: {
+        isAvailableForPurchase: false,
         name: data.name,
         description: data.description,
         priceInCents: data.priceInCents,
-        filePath: ,
-        imagePath:
+        filePath,
+        imagePath
     }})
+
+    redirect ("/admin/products")
 }
