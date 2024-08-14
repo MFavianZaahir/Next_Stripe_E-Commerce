76f17ed0c19824804575e03bff6e1574653d7f4c
MFavian Zaahir <favian.zahir@gmail.com>
2024-08-14 18:18:01 +0700

    created admin authorization(sorta) and started the home page for customers

---
+++ src/app/(customerFacing)/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import db from "@/db/db"
+import { Product } from "@prisma/client"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+
+function getMostPopularProducts() {
+    return db.product.findMany({ where : {isAvailableForPurchase: true}, 
+        orderBy: {orders: {_count: "desc"}}, take: 6 })
+}
+
+function getNewestProducts() {
+    return db.product.findMany({ where : {isAvailableForPurchase: true}, 
+        orderBy: {createdAt: "desc"}, take: 6 })
+}
+
+export default function HomePage() {
+    return( <main className="spacey-y-12">
+        <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProducts} />
+        <ProductGridSection title="Newest Products" productsFetcher={getNewestProducts} />
+    </main>
+    )
+}
+
+type ProductGridSectionProps = {
+    title: string,
+    productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({productsFetcher, title} : ProductGridSectionProps) {
+    return(
+        <div className="space-y-4">
+            <div className="flex gap-4">
+                <h2 className="text-3xl">{title}</h2>
+                <Button variant="outline" asChild>
+                    <Link href="/products" className="space-x-2"><span>View All</span>
+                    <ArrowRight className="gap-4"></ArrowRight>
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    )
+}
