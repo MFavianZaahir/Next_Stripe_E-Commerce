470ef837cc2058c3fd50c594870af23a9f3b0a00
MFavian Zaahir <favian.zahir@gmail.com>
2024-08-19 22:26:46 +0700

    added stripe

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,13 +1,25 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe"
+import { CheckoutForm } from "./_components/CheckoutForm";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({ params: { id } }: { params: { id: string } }) {
     const product = await db.product.findUnique({where: {id} });
     if (!product || product == null) {
         return notFound()
     }
-    return <div>PurchasePage</div>;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount:product.priceInCents,
+        currency: "USD",
+        metadata: {productId: product.id}
+    })
+
+    if  (paymentIntent.client_secret == null) {
+        return notFound()
+    }
+
+    return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret} />
 }
