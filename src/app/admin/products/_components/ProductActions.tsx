7c5eb0a07aea728aa407b3ad2d224879adc2f8eb
MFavian Zaahir <favian.zahir@gmail.com>
2024-08-24 19:19:01 +0700

    trying to solve why the product sell count doesn't go up and didn't receive an email after making a purchase

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -1,41 +1,58 @@
 "use client"
 
-import { useTransition } from "react";
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { deleteProduct, toggleProductAvailability } from "@/app/admin/_actions/products";
-import { useRouter } from "next/navigation";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { useTransition } from "react"
+import {
+  deleteProduct,
+  toggleProductAvailability,
+} from "../../_actions/products"
+import { useRouter } from "next/navigation"
 
-export function ActiveToggleDropdownItem({id, isAvailableForPurchase} : {id: string, isAvailableForPurchase: boolean}) {
-    const [isPending, startTransition] = useTransition() 
-    const router = useRouter()
-    return (
+export function ActiveToggleDropdownItem({
+  id,
+  isAvailableForPurchase,
+}: {
+  id: string
+  isAvailableForPurchase: boolean
+}) {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+  return (
     <DropdownMenuItem
-    disabled={isPending} onClick={() => {
-            startTransition(async () => {
-                await toggleProductAvailability(id, !isAvailableForPurchase)
-                router.refresh()
-                router.push(router.asPath) //goofy ahh but it works
-            })
-        }}>
-            {isAvailableForPurchase ? "Deactivate" : "Activate"}
+      disabled={isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await toggleProductAvailability(id, !isAvailableForPurchase)
+          router.refresh()
+        })
+      }}
+    >
+      {isAvailableForPurchase ? "Deactivate" : "Activate"}
     </DropdownMenuItem>
-    )
+  )
 }
 
-export function DeleteDropdownItem({ id, disabled } : { id: string, disabled: boolean }){
-    const [isPending, startTransition] = useTransition() 
-    const router = useRouter()
-    return (
+export function DeleteDropdownItem({
+  id,
+  disabled,
+}: {
+  id: string
+  disabled: boolean
+}) {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+  return (
     <DropdownMenuItem
-    variant="destructive"
-    disabled={disabled || isPending} onClick={() => {
-            startTransition(async () => {
-                await deleteProduct(id)
-                router.refresh()
-                router.push(router.asPath) //goofy ahh but it works
-            })
-        }}>
-            Delete
+      variant="destructive"
+      disabled={disabled || isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await deleteProduct(id)
+          router.refresh()
+        })
+      }}
+    >
+      Delete
     </DropdownMenuItem>
-    )
+  )
 }
